b8a4e340589eb19c8675d4f804657d81ee22cba0
Reinaldo Gonzalez <reinaldoagf1@gmail.com>
2022-06-14 20:43:53 -0400

    Spinner de carga de articulos

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles/Spinner.css';
+
+const ArticleItem = ({ article }) => {
+	return (
+		<div class="lds-ring"><div></div><div></div><div></div><div></div></div>
+	);
+}
+
+export default ArticleItem;
--- src/containers/ArticleList.jsx
+++ src/containers/ArticleList.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
+import Spinner from '../components/Spinner';
 import ArticleItem from '../components/ArticleItem';
 import useGetArticles from '../hooks/useGetArticles';
 
 const API = 'https://5eed24da4cbc340016330f0d.mockapi.io/api';
 
 const ArticleList = ({query}) => {
-    console.log(query)
-    const articles = useGetArticles(`${API}/${query}` )
+    const {loading, articles} = useGetArticles(`${API}/${query}`)
     console.log(articles)
+    console.log(loading)
     return (
         <section className="articles-container">
-            {
+            {loading && <Spinner />}
+            { !loading &&
                 articles.map(e => (
                     <ArticleItem article={e} key={e.id} />
                 ))
